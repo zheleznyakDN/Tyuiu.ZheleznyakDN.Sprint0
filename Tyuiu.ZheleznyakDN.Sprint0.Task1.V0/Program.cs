40be6720a4136466f2f78c42637fac3fa15ef2df
Железняк Дамир <Tyuiu.ZheleznyakDN@outlook.com>
2025-09-09 21:37:51 +0500

    Task1

---
+++ Tyuiu.ZheleznyakDN.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ZheleznyakDN.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Дамир";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
