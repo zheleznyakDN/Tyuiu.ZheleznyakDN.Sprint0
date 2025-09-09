eead59faab20c96e8da3761612c3fb9fffe8ba16
Дамир <@DESKTOP-42NPHSR>
2025-09-09 20:58:19 +0500

    Add project files.

---
+++ Tyuiu.ZheleznyakDN.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ZheleznyakDN.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
